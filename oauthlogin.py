e7fc791583a88fe6b9f8c323f47017d4a28f941e
anil19584 <anil19584@gmail.com>
2025-02-27 16:32:05 +0530

    added file of oauth

---
+++ oauthlogin.py
@@ -0,0 +1,5 @@
+client_id = "3MVG9GCMQoQ6rpzT5wJh2oXJwo5GAP36PQnPC7jdkAUskaQwSTY5.DdzihynyuqfFJwM8jQkw3ez2qlcVvv.1"
+client_secret = "DF7EF36FB4A31732E38F4DE68DAEE4972BA57DACA4C547C0AB12553179C8CAC0"
+
+# Salesforce OAuth 2.0 endpoint for token requests
+token_url = 'https://mindful-goat-p5c3v5-dev-ed.trailblaze.my.salesforce.com/services/oauth2/token'
